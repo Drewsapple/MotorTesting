95aa2b7acaa9e531f16bcaf8ed2029222029b3c7
Drew Fisher <drewsapple111@gmail.com>
2018-06-02 00:11:51 -0400

    Re-Order file for readablility
    Signed-off-by: Drew Fisher <drewsapple111@gmail.com>

--- QuickAnalysis.r
+++ QuickAnalysis.r
@@ -1,9 +1,10 @@
 args <- commandArgs(TRUE)
 motorSample <- read.csv(args[1]) 
 
-linearModel <- lm( RPM ~ Voltage, data=motorSample)
 print("Linear Model for free spinning systems")
+linearModel <- lm( RPM ~ Voltage, data=motorSample)
 print(summary(linearModel))
-inverseMult <- lm( Voltage ~ RPM + RPM.per.s, data=motorSample)
+
 print("Multiple Regression Model for higher inertia systems")
+inverseMult <- lm( Voltage ~ RPM + RPM.per.s, data=motorSample)
 print(summary(inverseMult))
