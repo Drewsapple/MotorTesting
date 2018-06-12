3ee3efa2bd19b17a1de31a9f135738e311092a4c
Drew Fisher <drewsapple111@gmail.com>
2018-06-12 10:14:17 -0400

    Make Trendline Clearer
    Signed-off-by: Drew Fisher <drewsapple111@gmail.com>

--- QuickAnalysis.r
+++ QuickAnalysis.r
@@ -1,10 +1,18 @@
 args <- commandArgs(TRUE)
 motorSample <- read.csv(args[1]) 
+png(file = args[2])
+
+plot(x = motorSample$Voltage, y = motorSample$RPM, xlim = c(-12, 12), ylim = c(-6000, 6000))
+print(summary(motorSample))
 
 print("Linear Model for free spinning systems")
 linearModel <- lm( RPM ~ Voltage, data=motorSample)
 print(summary(linearModel))
 
+abline(linearModel, col='red')
+
 print("Multiple Regression Model for higher inertia systems")
 inverseMult <- lm( Voltage ~ RPM + RPM.per.s, data=motorSample)
 print(summary(inverseMult))
+
+dev.off()
